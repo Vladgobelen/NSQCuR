f7c5171ace9ed4b88acc1b4a65182de957ab7ecf
Vladgobelen <vladgobelen@gmail.com>
2025-03-03 17:56:00 +0700

    03n17

--- src/main.rs
+++ src/main.rs
@@ -27,17 +27,19 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "Night Watch Updater",
         options,
-        Box::new(|cc| Box::new(App::new(cc))),
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
     )
 }
 
 fn load_icon() -> Option<IconData> {
     let icon_bytes = include_bytes!("../resources/emblem.ico");
     let image = image::load_from_memory(icon_bytes).ok()?.to_rgba8();
+    let (width, height) = (image.width(), image.height());
+    let rgba = image.into_raw();
 
     Some(IconData {
-        rgba: image.into_raw(),
-        width: image.width(),
-        height: image.height(),
+        rgba,
+        width,
+        height,
     })
 }
